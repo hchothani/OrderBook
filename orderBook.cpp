da3bb58501031d0c8f5b792d3a535cece3651581
hchothani <harshdchothani@gmail.com>
2025-05-26 16:46:26 +0530

    Add OrdeBook Class

--- orderBook.cpp
+++ orderBook.cpp
@@ -1,3 +1,4 @@
+#include <functional>
 #include <iostream>
 #include <map>
 #include <set>
@@ -78,6 +79,7 @@ class Order{
     Quantity GetInitialQuantity() const {return initialQuantity_;}
     Quantity GetRemainingQuantity() const {return remainingQuantity_;}
     Quantity GetFilledQuantity() const {return GetInitialQuantity() - GetRemainingQuantity();}
+    bool isFilled() const {return GetRemainingQuantity() == 0;}
     void Fill(Quantity quantity){
       if(quantity > GetRemainingQuantity())
         throw std::logic_error(std::format("Order ({}) cannot be filled for more than remaining quantity", GetOrderId()));
@@ -120,6 +122,141 @@ class OrderModify{
     Side side_;
     Quantity quantity_;
   
+};
+
+struct TradeInfo{
+  OrderId orderId_;
+  Price price_;
+  Quantity quantity_;
+};
+
+class Trade{
+  public:
+    Trade(const TradeInfo& bidTrade, const TradeInfo& askTrade)
+      : bidTrade_{bidTrade}
+      , askTrade_{askTrade}
+    {}
+
+    const TradeInfo& GetBidTrade() const {return bidTrade_;}
+    const TradeInfo& GetAskTrade() const {return askTrade_;}
+
+  private:
+    TradeInfo bidTrade_;
+    TradeInfo askTrade_;
+};
+
+using Trades = std::vector<Trade>;
+
+class OrderBook{
+  private:
+    struct OrderEntry{
+      OrderPointer order_{nullptr};
+      OrderPointers::iterator location_;
+    };
+
+    std::map<Price, OrderPointers, std::greater<Price>>bids_;
+    std::map<Price, OrderPointers, std::less<Price>>asks_;
+    std::unordered_map<OrderId, OrderEntry>orders_;
+
+    bool CanMatch(Side side, Price price) const{
+      if(side == Side::Buy){
+        if(asks_.empty()) return false;
+
+        const auto& [bestAsk, _] = *asks_.begin();
+        return price >= bestAsk;
+      }
+      else{
+        if(bids_.empty()) return false;
+
+        const auto& [bestBid, _] = *bids_.begin();
+        return price <= bestBid;
+      }
+    };
+
+    Trades MatchOrders(){
+
+      Trades trades;
+      trades.reserve(orders_.size());
+
+      while(true){
+        if(bids_.empty() || asks_.empty()) break;
+        auto& [bidPrice, bids] = *bids_.begin();
+        auto& [askPrice, asks] = *asks_.begin();
+
+        if(bidPrice < askPrice) break;
+
+        while(bids.size() && asks.size()){
+          auto bid = bids.front();
+          auto ask = asks.front();
+
+          Quantity quantity = std::min(bid->GetRemainingQuantity(), ask->GetRemainingQuantity());
+
+          bid->Fill(quantity);
+          ask->Fill(quantity);
+
+          if(bid->isFilled()){
+            bids.pop_front();
+            orders_.erase(bid->GetOrderId());
+          }
+
+          if(ask->isFilled()){
+            asks.pop_front();
+            orders_.erase(ask->GetOrderId());
+          }
+
+          if(bids.empty()) bids_.erase(bidPrice);
+          if(asks.empty()) asks_.erase(askPrice);
+
+          trades.push_back(Trade{
+          TradeInfo{bid->GetOrderId(), bid->GetPrice(), quantity},
+          TradeInfo{ask->GetOrderId(), ask->GetPrice(), quantity},
+        });
+        
+        }
+      }
+
+      if(!bids_.empty()){
+        auto& [_, bids] = *bids_.begin();
+        auto& order = bids.front();
+        if(order->GetOrderType() == OrderType::FillAndKill)
+          CancelOrder(order->GetOrderId());
+
+      }
+      if(!asks_.empty()){
+        auto& [_, asks] = *asks_.begin();
+        auto& order = asks.front();
+        if(order->GetOrderType() == OrderType::FillAndKill)
+          CancelOrder(order->GetOrderId());
+    
+      }
+      return trades;
+    };
+
+  public:
+
+    Trades AddOrder(OrderPointer order){
+      if(orders_.contains(order->GetOrderId())) return { };
+
+      if(order->GetOrderType() == OrderType::FillAndKill && !CanMatch(order->GetSide(), order->GetPrice()))
+        return { };
+
+      OrderPointers::iterator iterator;
+
+      if(order->GetSide() == Side::Buy){
+        auto& orders = bids_[order->GetPrice()];
+        orders.push_back(order);
+        iterator = std::next(orders.begin(), orders.size() - 1);
+      }
+      else{
+        auto& orders = asks_[order->GetPrice()];
+        orders.push_back(order);
+        iterator = std::next(orders.begin(), orders.size() - 1);
+      }
+
+      orders_.insert({order->GetOrderId(), OrderEntry{order, iterator}});
+      return MatchOrders();
+    }
+
 };
 
 int main(){
